bbc80a8c5b58873890d5e7d64b8b1bfa19ab7b80
IAmrinderSingh <amrindersingh1703@gmail.com>
2021-11-17 18:48:29 +0530

    checking password if it exits and looping it

--- pass_check.py
+++ pass_check.py
@@ -8,3 +8,18 @@ def request_api_data(query_char):
     if res.status_code!=200:
         raise RuntimeError(f'Error fetching:{res.status_code},')
     return res
+
+def get_pass_leaks_count(hashes,hash_to_check):
+    
+    hashes=(line.split(':') for line in hashes.text.splitlines())
+    for h,count in hashes:
+        if h==hash_to_check:
+            return count
+    return 0
+
+def pwned_api_check(password):
+    #check password if it exits in api response
+    sha1password=hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5char,tail=sha1password[:5],sha1password[5:]
+    response=request_api_data(first5char) # using first 5 char of hash
+    return get_pass_leaks_count(response,tail)
